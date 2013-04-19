03e4aad93f4608e84fd7cc4ec8568d23bc8319ec
Lyndon Maydwell <lyndon@sordina.net>
2013-04-19 14:22:10 +0800

    Test-Harness working

--- src/TestSuite.hs
+++ src/TestSuite.hs
@@ -1,8 +1,31 @@
-module Main where
+import Test.Framework (Test)
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
 
-import System.Exit (exitFailure)
+main :: IO () 
+main = defaultMain tests
 
-main = do
-    putStrLn "This test always fails!"
-    exitFailure
+ignore :: Functor f => f a -> f () 
+ignore = fmap (const ())
 
+numbering :: [String]
+numbering = map (("Test " ++) . (show :: Int -> String)) [1..]
+
+dummyProperties :: [Test]
+dummyProperties = zipWith testProperty numbering [ \ a -> (*2) a == ((*2) a :: Int)
+                                                 , \ a -> (*2) a == ((*2) $ a :: Int)
+                                                 -- , \ a -> (*3) a == ((*2) $ a :: Int) -- Failing dummy test
+                                                 ]
+
+tests :: [Test]
+tests = [ testGroup "dummyProperties" dummyProperties
+        ]
+
+-- Notes:
+
+-- import Test.Framework.Providers.HUnit
+-- import Prelude hiding ((||),(&&))
+-- import Test.QuickCheck hiding ((==>))
+-- import Test.HUnit hiding (Test)
+
+-- testGroup "cases"      $ zipWith testCase     numbering $ [] ,
