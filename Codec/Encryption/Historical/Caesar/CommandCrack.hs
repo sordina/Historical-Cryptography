db74e908e15ff8fc8bf5d2409d8625575259bcbb
Lyndon Maydwell <lyndon@sordina.net>
2013-04-21 20:24:11 +0800

    Command line options processing

--- Codec/Encryption/Historical/Caesar/CommandCrack.hs
+++ Codec/Encryption/Historical/Caesar/CommandCrack.hs
@@ -3,12 +3,19 @@ module Main where
 
 import Codec.Encryption.Historical.Caesar.Analysis
 import Codec.Encryption.Historical.Caesar.Implementation
+import System.Environment
 
 main :: IO ()
-main = do
+main = getArgs >>= resolve
 
+resolve :: [String] -> IO ()
+
+resolve ["--help"] = usage
+resolve ["-h"] = usage
+
+resolve [f] = do
   cypher <- getContents
-  corpus <- readFile "/usr/share/dict/snow_white_abridged.txt" -- TODO: Allow user to specify
+  corpus <- readFile f
 
   let
     stripped_corpus = caesar_encode 0 corpus
@@ -16,3 +23,10 @@ main = do
     decrypted       = crack histo cypher
 
   putStrLn $ decrypted
+
+resolve [] = resolve ["/usr/share/dict/snow_white_abridged.txt"]
+
+resolve _ = usage
+
+usage :: IO ()
+usage = putStrLn "Usage: cat <cypher> | crack_caeser [corpus]"
